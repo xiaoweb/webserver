9bd14b3fae55fc8c4d54b2e457ff6c9f49ce4b73
xiaoweb <635156603@qq.com>
2013-09-09 14:28:50 +0800

    添加mysql

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,24 @@
 /*
  * GET home page.
  */
+var mysql=require('mysql');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    var connection = mysql.createConnection({
+        host:'db4free.org',
+        user:'z900815',
+        password:"zhou900815",
+        database:'xiaowebsql'
+    });
+    connection.connect();
+    connection.query('SELECT * FROM user',function(err,rows,fields){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(rows);
+            res.render('index', { title: 'Express','user':rows });
+        }
+        connection.end();
+    });
+
 };
