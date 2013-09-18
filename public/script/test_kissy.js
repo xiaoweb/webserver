c9f7434f7535d21e64360a0836383017fa9f99b7
xiaoweb <635156603@qq.com>
2013-09-18 18:30:22 +0800

    kissy学习

--- public/script/test_kissy.js
+++ public/script/test_kissy.js
@@ -5,46 +5,18 @@
  * Time: 上午11:57
  * To change this template use File | Settings | File Templates.
  */
-
-KISSY.use('node',function (S,Node) {
-    var $=Node.all;
-    var API = 'http://api.flickr.com/services/rest/?',
-        params = {
-            'method': 'flickr.favorites.getPublicList',
-            'api_key': '5d93c2e473e39e9307e86d4a01381266',
-            'user_id': '26211501@N07',
-            'per_page': 10,
-            'format': 'json',
-            'jsoncallback': 'getFavorites'
-        },
-        photoList = $('#photo-list');
-
-    $('#fetch-btn-anim').on('click', function() {
-        $(this).attr('disabled', true);
-        photoList.addClass('loading');
-        S.getScript(API + S.param(params));
-    });
-
-    window.getFavorites = function(data) {
-        var html = 'Fetch photo failed, pls try again!',
-            loading = true;
-
-        if (data.stat === 'ok') {
-            html = '';
-            S.each(data.photos.photo, function(item, i){
-                html += '<img style="display:none" src="http://farm' + item.farm + '.static.flickr.com/'
-                    + item.server + '/' + item.id + '_' + item.secret + '_t.jpg" />';
-            });
-        }
-
-        photoList.html(html).all('img').each(function(img) {
-            img.on('load', function() {
-                if(loading) {
-                    photoList.removeClass('loading');
-                    loading = false;
-                }
-                img.fadeIn(3);
-            });
+KISSY.use('button,dom',function(s,btn,dom){
+    s.ready(function(){
+        var b = new btn({
+            content: "这是按钮",
+            width: 200,
+            prefixCls: "liz-",
+            tooltip: "我有新皮肤",
+            value:"10",
+            render:"#edit"
         });
-    }
+
+        b.on('click',function(){alert(1)});
+        b.render();
+    });
 });
