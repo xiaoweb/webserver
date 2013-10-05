2364865e8aaa57e11f643d17a8c2f534808740f0
xiaoweb <635156603@qq.com>
2013-10-05 13:58:43 +0800

    添加mongoose并成功连接mongodb

--- routes/models.js
+++ routes/models.js
@@ -4,13 +4,27 @@
  * Date: 13-10-3
  * Time: 下午8:14
  */
-var mongoose=require("mongoose");
-var schema=mongoose.Schema;
+var mongoose=require('mongoose'),
+    schema=mongoose.Schema;
+mongoose.connect('mongodb://192.168.0.208/test',function(err){
+  err?console.log("连接出错 "+err):console.log("连接mongodb成功");
+});
 
+var db={};
 var _user=new schema({
   email:String,
+  password:String,
   name:String,
-  salt:String,
-  password:String
+  age:Number
 });
-exports.user=mongoose.model("user",_user);
+db.user=mongoose.model('user',_user);
+
+var _record=new schema({
+  time:Date,
+  name:String
+});
+db.record=mongoose.model('record',_record);
+
+
+exports.db=db;
+
--- routes/user.js
+++ routes/user.js
@@ -2,32 +2,26 @@
 /*
  * GET users listing.
  */
-var models=require("./models");
-var mongoose=require("mongoose");
-var User=models.user;
-mongoose.connect("mongodb://192.168.0.208/test",function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("已经连上mongodb");
-  }
-});
+var model=require('./models');
 
-
-
-exports.list = function(req, res){
-  /*var user=new User({
-    email:"635156603@qq.com",
-    name:"周礼颍"
+exports.list=function(req,res){
+  var one = new model.db.user({
+    email:'635156603@qq.com',
+    password:'123456',
+    name:'小明',
+    age:18
   });
-  user.save(function(){
-    res.send("成功保存<br/>"+user);
-  });*/
-  User.find({},function(err,data){
-    var name=[];
-    for(var i in data){
-      name.push(data[i].name);
-    }
-    res.render('user',{'name':name,'title':"用户列表"});
+  one.save(function(err,data){
+    var msg;
+    err?(msg='保存未成功'):(msg='已成功保存 '+data.name);
+    var record=new model.db.record({
+      time:Date.now(),
+      name:data.name
+    });
+    record.save();
+    res.render('user',{
+      'title':"保存数据返回页",
+      'msg':msg
+    });
   });
 };
