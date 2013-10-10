8b55562a7f0bc38596ac700eff1c4666f2169838
zhou <635156603@qq.com>
2013-10-10 18:45:23 +0800

    注册页

--- public/script/userjs/reg.js
+++ public/script/userjs/reg.js
@@ -4,6 +4,8 @@
  * Date: 13-10-9
  * Time: 下午9:21
  */
+
+/*校验状态*/
 $(".form-group").each(function(i,t){
     var t=$(t) ;
     $(t).find("input").blur(function(){
@@ -14,14 +16,43 @@ $(".form-group").each(function(i,t){
         }
     });
 });
+
+/*post提交*/
 $("#btn").click(function(){
-    if($("#inputPassword4").val() != "" && $("#inputPassword3").val() != "" && $("#inputEmail3").val() != "" && $("#inputName1").val() != ""){
-        $.post('/registration',function(data){
-            console.log(data);
-        });
-        return false;
+    var email=$("#inputEmail").val(),
+        password1=$("#inputPassword1").val(),
+        password2=$("#inputPassword2").val(),
+        name=$("#inputName").val();
+    if(email != "" && password1 != "" && password2 != "" && name != ""){
+      $("#btn").button('loading');
+      $.ajax({
+        url:'/registration',
+        type:'post',
+        data:{
+          email:email,
+          password1:password1,
+          password2:password2,
+          username:name
+        },
+        dataType:'json',
+        success:function(data){
+            $('#myModal').find('.modal-body').html(data.msg);
+            $('#myModal').modal();
+            $("#btn").button('reset');
+            if(data.suc){
+                $("#myModal").find(".close,.btn").one("click",function(){
+                    document.location.href="./";
+                });
+            }
+        },
+        error:function(){
+            $("#myModal").find('.modal-body').html('发送失败');
+            $('#myModal').modal();
+            $("#btn").button('reset');
+        }
+      });
     }else{
-        alert("HI!,你的信息没输入全吧！");
-        return false;
+        $('#myModal').find('.modal-body').html("HI!,亲！有些东西是不能少的");
+        $('#myModal').modal();
     }
 });
--- routes/user.js
+++ routes/user.js
@@ -5,25 +5,25 @@
 var model=require('./models');
 
 exports.list=function(req,res){
-  var one = new model.db.user({
-    email:'635156603@qq.com',
-    password:'123456',
-    name:'小明',
-    age:18
-  });
-  one.save(function(err,data){
-    var msg;
-    err?(msg='保存未成功'):(msg='已成功保存 '+data.name);
-    var record=new model.db.record({
-      time:Date.now(),
-      name:data.name
+    var one = new model.db.user({
+        email:'635156603@qq.com',
+        password:'123456',
+        name:'小明',
+        age:18
     });
-    record.save();
-    res.render('user',{
-      'title':"保存数据返回页",
-      'msg':msg
+    one.save(function(err,data){
+        var msg;
+        err?(msg='保存未成功'):(msg='已成功保存 '+data.name);
+        var record=new model.db.record({
+            time:Date.now(),
+            name:data.name
+        });
+        record.save();
+        res.render('user',{
+            'title':"保存数据返回页",
+            'msg':msg
+        });
     });
-  });
 };
 
 /*会员注册*/
@@ -31,28 +31,33 @@ exports.reg=function(req,res){
     res.render("registration",{'title':"帐号注册"});
 };
 exports.regpost=function(req,res){
-    var body= req.body.user;
-    if(req.body.user.password1 === req.body.user.password2){
+    var body= req.body;
+    if(body.password1 === body.password2){
         model.db.user.find({email:body.email},function(err,data){
-            if(data.length<=0){
-                var user=new model.db.user({
-                     email:body.email,
-                     password:body.password1,
-                     name:body.name,
-                     regtime:new Date()
-                });
-                 user.save(function(err){
-                     if(err){
-                        res.send('注册失败');
-                     }else{
-                        res.send('注册成功');
-                     }
-                 });
+            if(err){
+                console.log(err);
+                res.send({'msg':err});
             }else{
-                res.send('<script>document.location.href="http://www.baidu.com"</script>');
+                if(data.length<=0){
+                    var user=new model.db.user({
+                        email:body.email,
+                        password:body.password1,
+                        name:body.username,
+                        regtime:new Date()
+                    });
+                    user.save(function(err){
+                        if(err){
+                            res.send({'msg':'注册失败'});
+                        }else{
+                            res.send({'msg':'注册成功','suc':'true'});
+                        }
+                    });
+                }else{
+                    res.send({'msg':body.email+'已被注册'});
+                }
             }
         });
     }else{
-        res.send("两次密码输入的不一样，请重新输入");
+        res.send({'msg':"两次密码输入的不一样，请重新输入"});
     }
 };
