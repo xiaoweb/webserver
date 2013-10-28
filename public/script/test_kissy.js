b51f5f4718a83d7bce8ced4e69318e8a031798c0
xiaoweb <635156603@qq.com>
2013-10-28 18:41:47 +0800

    KISSY学习

--- public/script/test_kissy.js
+++ public/script/test_kissy.js
@@ -5,18 +5,12 @@
  * Time: 上午11:57
  * To change this template use File | Settings | File Templates.
  */
-KISSY.use('button,dom',function(s,btn,dom){
-    s.ready(function(){
-        var b = new btn({
-            content: "这是按钮",
-            width: 200,
-            prefixCls: "liz-",
-            tooltip: "我有新皮肤",
-            value:"10",
-            render:"#edit"
-        });
 
-        b.on('click',function(){alert(1)});
-        b.render();
+KISSY.use("xiaozhou,button",function(S,x,but){
+    var p =new but({
+        content:"a" ,
+        render: "body",
+        tooltip: "点击我有惊喜~"
     });
+    p.render();
 });
