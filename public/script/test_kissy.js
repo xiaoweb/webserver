e5463015c6038b8fe13557dcfb0689140bd2083c
xiaoweb <635156603@qq.com>
2013-11-25 09:46:50 +0800

    kissy slide

--- public/script/test_kissy.js
+++ public/script/test_kissy.js
@@ -5,41 +5,30 @@
  * Time: 上午11:57
  * To change this template use File | Settings | File Templates.
  */
-/*KISSY.use("base",function(S,b){
-    function test(){
-        this.x=10;
-    }
-    S.extend(test,b);
-    test.ATTRS={
-        size:{
-            value:10,
-            getter:function(x){
-                console.log(new Date());
-                return x+1;
-            },
-            setter:function(x){
-                return x;
-            },
-            validator:function(x){
-                if(x < 10){
-                    return false;
-                }
+function test() {
+    this.x = 10;
+}
+test.ATTRS = {
+    size: {
+        value: 10,
+        getter: function (x) {
+            console.log(new Date());
+            return x + 1;
+        },
+        setter: function (x) {
+            return x;
+        },
+        validator: function (x) {
+            if (x < 10) {
+                return false;
             }
         }
-    };
-    var obj=new test();
+    }
+};
+KISSY.use("base", function (S, b) {
 
-    obj.addAttr("name",{
-        value: test.ATTRS.size.value,
-        getter:test.ATTRS.size.getter,
-        validator:test.ATTRS.size.validator
-    });
-    obj.set("name",8);
-    console.log(obj.get("name"));
-});*/
-KISSY.use("node,event,dom",function(S,N,E,D){
-    var $= S.all;
-    var p=document.createElement("p");
-    D.insertAfter(p,"#node");
+    S.extend(test, b);
+
+    var obj = new test();
+
 });
-
